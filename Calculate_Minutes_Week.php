2810b0bdd05b7b14ed4be97c7fe63cf473ba1f21
Kenneth Ho <hsscosmos@hotmail.com>
2012-10-05 23:41:35 -0400

    first hw4 commit

---
+++ Calculate_Minutes_Week.php
@@ -0,0 +1,36 @@
+<?php
+$DaysPerWeek = 7;
+$MinutesPerHour = 60;
+/****************************************************************/
+/* Case 1: In our galaxy                                        */
+/****************************************************************/
+/* Initialize Hours per Day variable for Current Galaxy */
+
+$HoursPerDay = 24;
+
+
+
+/*************************************************************************/
+/*  $NumberMinutesWeek = $MinutesPerHour * $HoursPerDay * $DaysPerWeek;  */
+/*************************************************************************/
+$NumberMinutesWeek = $MinutesPerHour * $HoursPerDay * $DaysPerWeek;
+print ("The number of weeks in current galaxy is: $NumberMinutesWeek <br />");
+
+/****************************************************************/
+/* Case 2: In Bilky Way Galaxy                                  */
+/****************************************************************/
+/* Initialize Hours per Day variable for Bilky Way Galaxy */
+
+$HoursPerDay = 26;
+
+
+
+/*************************************************************************/
+/*  $NumberMinutesWeek = $MinutesPerHour * $HoursPerDay * $DaysPerWeek;  */
+/*************************************************************************/
+$NumberMinutesWeek = $MinutesPerHour * $HoursPerDay * $DaysPerWeek;
+print ("The number of weeks in Bilky Way Galaxy is: $NumberMinutesWeek <br />");
+
+
+		
+?>		
---
+++ print_identity.php
@@ -0,0 +1,12 @@
+<?php
+/* Initialize identity variables */
+$Name = "Kenneth Ho";
+$BirthDate = "December 15, 1959";
+$FavoriteColor = "Blue";
+
+/* print identity strings */
+print "My Name is: $Name <br \>";
+print "My Birthday is: $BirthDate <br \>";
+print "My favorite color is: $FavoriteColor <br \>";
+
+?>
